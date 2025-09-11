1878b76f10f41c71518dc07974543c8492ef87f6
Sudarsana25 <sudarsanaburagohain60@gmail.com>
2025-09-11 10:49:47 +0530

    Add files via upload

---
+++ variables2.cpp
@@ -0,0 +1,12 @@
+// initialisation of variables
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a=5;        //initial value = 5
+	int(2);         // initial value = 2
+	int result;     // initial value undetermined
+	a=a+3;
+	cout << result;
+	return 0;
+}
